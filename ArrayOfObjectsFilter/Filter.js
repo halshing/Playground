67c5b79794cf6ee25e6889aa6ad5780de034d437
Hasan <hasan.alshingieti@gmail.com>
2018-05-30 07:26:56 -0400

    Fixed count bug

--- ArrayOfObjectsFilter/Filter.js
+++ ArrayOfObjectsFilter/Filter.js
@@ -2,12 +2,13 @@
 Array.prototype.flexFilter = function(info) {
   
   // Set our variables
-  var matchesFilter, matches = [], count = 0;
+  var matchesFilter, matches = [], count;
   
   // Helper function to loop through the filter criteria to find matching values
   // Each filter criteria is treated as "AND". So each item must match all the filter criteria to be considered a match.
   // Multiple filter values in a filter field are treated as "OR" i.e. ["Blue", "Green"] will yield items matching a value of Blue OR Green.
   matchesFilter = function(item) {
+    count = 0
     for (var n = 0; n < info.length; n++) {
       if (info[n]["Values"].indexOf(item[info[n]["Field"]]) > -1) {
         count++;
@@ -19,14 +20,32 @@ Array.prototype.flexFilter = function(info) {
 
   // Loop through each item in the array
   for (var i = 0; i < this.length; i++) {
-
     // Determine if the current item matches the filter criteria
     if (matchesFilter(this[i])) {
       matches.push(this[i]);
     }
-
   }
 
   // Give us a new array containing the objects matching the filter criteria
   return matches;
 }
+
+var data = [
+  { ID: 1, Name: "John", Color: "Blue", Location: "Up" },
+  { ID: 2, Name: "Pauline", Color: "Green", Location: "Up" },
+  { ID: 3, Name: "Ahmed", Color: "Orange", Location: "Left" },
+  { ID: 4, Name: "Diego", Color: "Pink", Location: "Up" },
+  { ID: 5, Name: "Maria", Color: "Black", Location: "Down" },
+  { ID: 6, Name: "Gus", Color: "Green", Location: "Up" },
+  { ID: 7, Name: "Brian", Color: "Pink", Location: "Left" },
+  { ID: 8, Name: "Shelley", Color: "Green", Location: "Right" },
+  { ID: 9, Name: "Leonardo", Color: "Blue", Location: "Right" },
+  { ID: 10, Name: "Big Daddy", Color: "Green", Location: "Down" }
+];
+
+var criteria = [
+  { Field: "Color", Values: ["Green"] },
+  { Field: "Location", Values: ["Up", "Down"] }
+];
+var filtered = data.flexFilter(criteria);
+console.log(filtered);
